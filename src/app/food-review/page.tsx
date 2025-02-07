a6de2512e0495e91ff396d9f192a25a241db9936
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2025-02-07 17:27:18 +0800

    chore: updated README.md

--- src/app/food-review/page.tsx
+++ src/app/food-review/page.tsx
@@ -8,56 +8,51 @@ import useFetchUserClient from "@/hooks/useFetchUserClient";
 import SortBy from "./components/SortBy";
 
 export default function Page() {
-	const {
-		reviews,
-		loading,
-		addReview,
-		deleteReview,
-		updateReview,
+  const {
+    reviews,
+    loading,
+    addReview,
+    deleteReview,
+    updateReview,
 
-		setSortBy,
-	} = useReviews();
-	const [showModal, setShowModal] = useState(false);
-	const { user } = useFetchUserClient();
+    setSortBy,
+  } = useReviews();
+  const [showModal, setShowModal] = useState(false);
+  const { user } = useFetchUserClient();
 
-	const handleButtonClick = () => {
-		setShowModal(true);
-	};
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
 
-	const handleCloseModal = () => {
-		setShowModal(false);
-	};
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
-	return (
-		<div className="h-screen overflow-y-scroll w-full relative ">
-			<div className=" sticky top-0 left-0 z-10 flex justify-between p-2">
-				<AddReviewButton onClick={handleButtonClick} />
-				<SortBy setSortBy={setSortBy} />
-			</div>
+  return (
+    <div className="h-screen overflow-y-scroll w-full relative ">
+      <div className=" sticky top-0 left-0 z-10 flex justify-between p-2">
+        <AddReviewButton onClick={handleButtonClick} />
+        <SortBy setSortBy={setSortBy} />
+      </div>
 
-			{showModal && (
-				<Modal
-					onClose={handleCloseModal}
-					addReview={addReview}
-				/>
-			)}
+      {showModal && <Modal onClose={handleCloseModal} addReview={addReview} />}
 
-			<div className="mt-4 flex flex-col items-center">
-				{loading && <p>Loading reviews...</p>}
-				{!loading && reviews.length === 0 && (
-					<p>No reviews found. Please add a review.</p>
-				)}
-				{!loading &&
-					reviews.map((review) => (
-						<ReviewCard
-							key={review.id}
-							review={review}
-							isCreator={review.user_id === user?.id}
-							deleteReview={deleteReview}
-							updateReview={updateReview}
-						/>
-					))}
-			</div>
-		</div>
-	);
+      <div className="mt-4 flex flex-col items-center">
+        {loading && <p>Loading reviews...</p>}
+        {!loading && reviews.length === 0 && (
+          <p>No reviews found. Please add a review.</p>
+        )}
+        {!loading &&
+          reviews.map((review) => (
+            <ReviewCard
+              key={review.id}
+              review={review}
+              isCreator={review.user_id === user?.id}
+              deleteReview={deleteReview}
+              updateReview={updateReview}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
